ac82014b1229ad2fa4b9c8ff8ac1944a1f8af2a2
freddyalfonsoboulton <freddyalfonsoboulton@gmail.com>
2018-02-01 20:47:42 -0600

    Adding attention as well as parametrized dropout

--- toxic/model_utils.py
+++ toxic/model_utils.py
@@ -6,7 +6,8 @@ from keras.optimizers import Nadam
 
 
 def get_model(sequence_len, dense_shape, word_embeddings_matrix, lstm_dim,
-              RNN=LSTM):
+              dropout_rate, RNN=LSTM):
+    """Vanilla architecture for LSTM based on word embeddings."""
     text_input = Input(shape=(sequence_len,))
     embedding_layer = Embedding(word_embeddings_matrix.shape[0],
                                 word_embeddings_matrix.shape[1],
@@ -14,10 +15,31 @@ def get_model(sequence_len, dense_shape, word_embeddings_matrix, lstm_dim,
                                 weights=[word_embeddings_matrix])
     x = embedding_layer(text_input)
     x = Bidirectional(RNN(lstm_dim, return_sequences=False))(x)
-    x = Dropout(0.3)(x)
+    x = Dropout(dropout_rate)(x)
     x = Dense(dense_shape)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
+    model_prediction = Dense(6, activation='sigmoid')(x)
+    model = Model(inputs=[text_input], outputs=[model_prediction])
+    model.compile(optimizer=Nadam(lr=0.001),
+                  loss='binary_crossentropy',
+                  metrics=['acc'])
+    return model
+
+
+def get_model_attention(sequence_len, dense_shape, word_embeddings_matrix,
+                        lstm_dim, dropout_rate, RNN=LSTM):
+    """Model that attends to first RNN's sequence."""
+    text_input = Input(shape=(sequence_len,))
+    embedding_layer = Embedding(word_embeddings_matrix.shape[0],
+                                word_embeddings_matrix.shape[1],
+                                trainable=False,
+                                weights=[word_embeddings_matrix])
+    x = embedding_layer(text_input)
+    x = Bidirectional(RNN(lstm_dim, return_sequences=True))(x)
+    x = Dropout(dropout_rate)(x)
+    x = Bidirectional(RNN(lstm_dim, return_sequences=False))(x)
+    x = Dense(dense_shape, activation='relu')(x)
     model_prediction = Dense(6, activation='sigmoid')(x)
     model = Model(inputs=[text_input], outputs=[model_prediction])
     model.compile(optimizer=Nadam(lr=0.001),
--- train_script.py
+++ train_script.py
@@ -2,7 +2,7 @@ from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint,\
                             EarlyStopping
 from keras.layers import GRU, LSTM
 import argparse
-from toxic.model_utils import get_model
+from toxic.model_utils import get_model, get_model_attention
 import numpy as np
 import pickle
 
@@ -15,41 +15,49 @@ def main():
     parser.add_argument("train_file_path")
     parser.add_argument("val_file_path")
     parser.add_argument("embedding_path")
+    parser.add_argument("--attention", default='No')
     parser.add_argument("--result-path", default="./results/")
-    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--batch-size", type=int, default=128)
     parser.add_argument("--sentences-length", type=int, default=200)
-    parser.add_argument("--recurrent-units", type=int, default=60)
-    parser.add_argument("--dense-size", type=int, default=30)
+    parser.add_argument("--recurrent-units", type=int, default=30)
+    parser.add_argument("--dense-size", type=int, default=20)
     parser.add_argument("--rnn", default='LSTM')
+    parser.add_argument("--dropout", type=float, default=0.2)
 
     args = parser.parse_args()
 
     rnn_dict = {"LSTM": LSTM, "GRU": GRU}
+    attention_dict = {'No': get_model, 'Yes': get_model_attention}
 
     train_archive = np.load(args.train_file_path)
     val_archive = np.load(args.val_file_path)
     embedding_weights = np.load(args.embedding_path)['weights']
 
-    train_text, train_targets = train_archive['text'], train_archive['targets']
-    val_text, val_targets = val_archive['text'], val_archive['targets']
+    train_text, train_targets = train_archive['text'],\
+                                train_archive['targets']
+    val_text, val_targets = val_archive['text'],\
+                            val_archive['targets']
 
-    model = get_model(sequence_len=args.sentences_length,
-                      dense_shape=args.dense_size,
-                      word_embeddings_matrix=embedding_weights,
-                      lstm_dim=args.recurrent_units,
-                      rnn=rnn_dict[args.rnn])
+    func = attention_dict[args.attention]
+
+    model = func(sequence_len=args.sentences_length,
+                 dense_shape=args.dense_size,
+                 word_embeddings_matrix=embedding_weights,
+                 lstm_dim=args.recurrent_units,
+                 rnn=rnn_dict[args.rnn],
+                 dropout=args.dropout)
 
     checkpoint = ModelCheckpoint(filepath=args.result_path + "weights/" +
-                                          "weights.{epoch:02d}-{val_loss:.2f}.hdf5",
+                                 "weights.{epoch:02d}-{val_loss:.2f}.hdf5",
                                  save_best_only=True,
                                  save_weights_only=True,
                                  verbose=1)
     es = EarlyStopping(patience=2, verbose=1)
     print("Training Model...")
     history = model.fit(train_text, train_targets, epochs=10,
-                         batch_size=args.batch_size,
-                         validation_data=(val_text, val_targets),
-                         callbacks=[checkpoint, ReduceLROnPlateau(), es])
+                        batch_size=args.batch_size,
+                        validation_data=(val_text, val_targets),
+                        callbacks=[checkpoint, ReduceLROnPlateau(), es])
 
     print("Saving History")
     pickle.dump(history.history, open(args.result_path + "history/" +
